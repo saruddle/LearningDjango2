d6418e43d26bcdb70cb090d7c5516130dfbd6391
Scott Ruddle <saruddle@hotmail.com>
2018-07-12 10:43:27 -0400

    Add project files.

---
+++ BasicProject2/BasicProject2/urls.py
@@ -0,0 +1,21 @@
+"""
+Definition of urls for BasicProject2.
+"""
+
+from django.conf.urls import include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', BasicProject2.views.home, name='home'),
+    # url(r'^BasicProject2/', include('BasicProject2.BasicProject2.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+]
