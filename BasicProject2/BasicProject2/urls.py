7d1a6aace1b9730f1fffdea29d64370f477fad89
Scott Ruddle <saruddle@hotmail.com>
2018-07-12 17:01:51 -0400

    Added HelloDjangoApp

--- BasicProject2/BasicProject2/urls.py
+++ BasicProject2/BasicProject2/urls.py
@@ -3,12 +3,15 @@ Definition of urls for BasicProject2.
 """
 
 from django.conf.urls import include, url
+from HelloDjangoApp.views import index
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = [
+    url(r'^$', index, name = 'index'),
+    url(r'^home$', index, name = 'home'),
     # Examples:
     # url(r'^$', BasicProject2.views.home, name='home'),
     # url(r'^BasicProject2/', include('BasicProject2.BasicProject2.urls')),
