7a14274734425630b5aaf6af57301fbcc6a48ae2
Scott Ruddle <saruddle@hotmail.com>
2018-07-12 17:43:41 -0400

    Updated the HelloDjangoApp view and template folder including index.html

--- BasicProject2/HelloDjangoApp/views.py
+++ BasicProject2/HelloDjangoApp/views.py
@@ -1,5 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from datetime import datetime
+
+from django.shortcuts import render   # Added for this step
+
 def index(request):
-    return HttpResponse("Hello, Django!")
+    now = datetime.now()
+
+    return render(
+        request,
+        "HelloDjangoApp/index.html",  # Relative path from the 'templates' folder to the template file
+        {
+            'title': "Hello Django",
+            'content': "Hello Django!", 
+            'message': " on " + now.strftime("%A, %d %B, %Y at %X")
+        }
+    )
