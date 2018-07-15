738207b2e39c0b397d1922540b004cbc7dc9ed62
Scott Ruddle <saruddle@hotmail.com>
2018-07-15 13:40:53 -0400

    DjangoPolls added

--- DjangoPolls/DjangoPolls/urls.py
+++ DjangoPolls/DjangoPolls/urls.py
@@ -1,24 +1,22 @@
 """
-Definition of urls for DjangoWeb.
+Definition of urls for DjangoPolls.
 """
 
 from datetime import datetime
-from django.conf.urls import url
+from django.conf.urls import url, include
+from django.contrib import admin
 import django.contrib.auth.views
 
 import app.forms
 import app.views
 
-# Uncomment the next lines to enable the admin:
-from django.conf.urls import include
-from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = [
-    # Examples:
-    url(r'^$', app.views.home, name='home'),
+    url(r'^', include('app.urls', namespace="app")),
     url(r'^contact$', app.views.contact, name='contact'),
     url(r'^about', app.views.about, name='about'),
+    url(r'^seed', app.views.seed, name='seed'),
     url(r'^login/$',
         django.contrib.auth.views.login,
         {
@@ -39,8 +37,7 @@ urlpatterns = [
         name='logout'),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', admin.site.urls),
+    url(r'^admin/', include(admin.site.urls)),
 ]
